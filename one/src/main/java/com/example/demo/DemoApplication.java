b450849b04881db3cc063bd3f5fdc64d4660750e
陈 <864117967@qq.com>
2020-03-12 18:20:48 +0800

    第二次修改

--- one/src/main/java/com/example/demo/DemoApplication.java
+++ one/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("第一次修改");
+
+        System.out.println("123456");
     }
 
 }
